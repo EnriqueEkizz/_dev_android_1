efc0a01a337bf1f1f20d7b55c5e889a0b2b373f0
Carlos Colque <enriqueekizz86@gmail.com>
2018-10-24 21:42:19 -0500

    Continuando...

---
+++ app/src/main/java/com/example/enrique/organizadorcomposicion/Entities/clsHarmonyBlock.java
@@ -0,0 +1,47 @@
+package com.example.enrique.organizadorcomposicion.Entities;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class clsHarmonyBlock {
+    private String Recording = "";
+    private ArrayList<String> HarmonyNotes;
+
+    public void setRecording(String xRecording) {
+        this.Recording = xRecording;
+    }
+    public void deleteRecording() {
+        this.Recording = "";
+    }
+
+    public void addHarmonyNotes(String xNotes) {
+        this.HarmonyNotes.add(xNotes);
+    }
+    public void deleteHarmonyNotes(int index) {
+        this.HarmonyNotes.remove(index);
+    }
+    public void updateHarmonyNotes(int index, String xNotes){
+        this.HarmonyNotes.set(index, xNotes);
+    }
+
+    public JSONObject getJsonBlock() {
+        JSONObject jsonObject = new JSONObject();
+        //Grabacion de bloque
+        try {
+            JSONArray arrayNotes = new JSONArray();
+            for (String notes : HarmonyNotes) {
+                arrayNotes.put(notes);
+            }
+            
+            jsonObject.put("RECORD", Recording);
+            jsonObject.put("NOTES", arrayNotes);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return jsonObject;
+    }
+}
---
+++ app/src/main/java/com/example/enrique/organizadorcomposicion/Entities/clsProjectStructure.java
@@ -0,0 +1,45 @@
+package com.example.enrique.organizadorcomposicion.Entities;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class clsProjectStructure {
+    private ArrayList<String> Recordings;
+    private ArrayList<clsHarmonyBlock> HarmonyBlocks;
+
+    public void addRecording(String xRecording) {
+        this.Recordings.add(xRecording);
+    }
+    public void deleteRecording(int index) {
+        this.Recordings.remove(index);
+    }
+
+    public void addHarmonyBlock(clsHarmonyBlock xHarmonyBlock) {
+        this.HarmonyBlocks.add(xHarmonyBlock);
+    }
+
+    public void getJsonForSave() {
+        JSONObject jsonObject = new JSONObject();
+
+
+        try {
+            // CONSEGUIR GRABACIONES
+            JSONArray arrRecorded = new JSONArray();
+            for (String recorded : Recordings) {
+                arrRecorded.put(recorded);
+            }
+            // CONSEGUIR BLOQUES
+            JSONArray arrBlocks = new JSONArray();
+            for (clsHarmonyBlock harmonyBlock : HarmonyBlocks) {
+                arrBlocks.put(harmonyBlock.getJsonBlock());
+            }
+
+            jsonObject.put("RECORDINGS", arrRecorded);
+            jsonObject.put("BLOCKS", arrBlocks);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
